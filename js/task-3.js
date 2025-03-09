48b186961e86c0aa68b04337d2f7caa5847df455
EV <evgenviktorov88@gmail.com>
2025-03-09 12:25:10 +0200

    task-3-done

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,10 @@
 console.log('-- task-3 --');
 
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+
+  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+}
 
 console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
